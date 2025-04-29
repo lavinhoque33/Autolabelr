76b909f18c2f67e7df6b7c4f57516207cf742b30
Iram Tazim Hoque <caspianprince33@yahoo.com>
2025-04-29 14:47:06 -0500

    Switch to lighter  deepseek-coder-1.3b-instruct model

--- inference/app.py
+++ inference/app.py
@@ -5,8 +5,11 @@ from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 app = FastAPI()
 
 # Load model on startup
-tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct")
-model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct", device_map="auto")
+# Switch to lighter model
+model_name = "deepseek-ai/deepseek-coder-1.3b-instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 class LabelRequest(BaseModel):
@@ -15,5 +18,9 @@ class LabelRequest(BaseModel):
 @app.post("/generate")
 async def generate_labels(data: LabelRequest):
     prompt = f"Label the following text with 4–6 keywords:\n\n{data.text}"
+    print(prompt)
+    # Generate labels
     response = pipe(prompt, max_new_tokens=100, do_sample=False)[0]['generated_text']
+    print('HERE')
+    print(response)
     return {"labels_raw": response}
